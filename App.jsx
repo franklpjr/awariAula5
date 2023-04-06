e1fe1d1fb0932bfbbfb5191f74d2a5cd452ab8b1
Frank Lopes <111025189+franklpjr@users.noreply.github.com>
2023-04-05 20:19:27 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import UserList from "./Components/UserList";
+
+function App() {
+
+  const [selected, setSelected] = useState(null);
+
+  const users = [
+    {
+      id: 0,
+      name: 'João',
+    },
+    {
+      id: 1,
+      name: 'Maria',
+    },
+    {
+      id: 2,
+      name: 'Thiago',
+    }
+  ]
+
+const handleOnItemClick = (user) => {
+  setSelected(user);
+}
+
+  return (
+    <div>
+      <h1>Usuários</h1>
+      { selected && (
+        <p>Selecionado: {selected.name}</p>
+      )}
+
+{/* No onItemClick abaixo, adicionei o ={handleOnItemClick} */}
+
+      <UserList users={users} onItemClick={handleOnItemClick} />
+
+      
+    </div>
+  );
+}
+
+export default App;
